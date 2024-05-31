bff55666c2d34481cb486953b2f4b3ccfb5ddd54
GregorioCavulla::ghigo <101806375+GregorioCavulla@users.noreply.github.com>
2024-05-31 10:34:52 +0200

    javaStream08

---
+++ Prove_Esame/Java Socket/08012020_Java/StreamClient.java
@@ -0,0 +1,155 @@
+import java.io.*;
+import java.net.*;
+
+public class StreamClient {
+    public static void main(String[] args) {
+
+        InetAddress addr = null; // indirizzo del server
+        int port = -1; // porta del server
+
+        Socket socket = null; // socket
+        DataInputStream inSock = null; // stream di input
+        DataOutputStream outSock = null; // stream di output
+
+        BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in)); // stream di input da tastiera
+
+        String buffer; // stringa per la comunicazione
+
+        // CONTROLLO ARGOMENTI
+
+        try {
+            if (args.length == 2) {
+                addr = InetAddress.getByName(args[0]);
+                port = Integer.parseInt(args[1]);
+                if (port < 1024 || port > 65535) {
+                    System.out.println("[ERROR] la porta deve essere compresa nel range valido [1024-65535 ");
+                    System.exit(1);
+                }
+            } else {
+                System.out.println("[ERROR] java StreamClient serverAddr serverPort ");
+                System.exit(1);
+            }
+        } catch (Exception e) {
+            System.out.println("[ERROR] Problemi, i seguenti: ");
+            e.printStackTrace();
+            System.out.println("java StreamClient serverAddr serverPort ");
+            System.exit(2);
+        }
+
+        // CREAZIONE SOCKET
+        try {
+            socket = new Socket(addr, port); // creazione socket
+            socket.setSoTimeout(300000); // timeout 5 minuti
+            System.out.println("[DEBUG] Creata la socket: " + socket + " sulla porta locale " + socket.getLocalPort());
+        } catch (Exception e) {
+            System.out.println("[ERROR] Problemi nella creazione della socket: ");
+            e.printStackTrace();
+            System.exit(1);
+
+        }
+        // CREAZIONE STREAM
+        try {
+            inSock = new DataInputStream(socket.getInputStream()); // creazione stream di input
+            outSock = new DataOutputStream(socket.getOutputStream()); // creazione stream di output
+        } catch (IOException e) {
+            System.out.println("[ERROR] Problemi nella creazione degli stream su socket: ");
+            e.printStackTrace();
+            System.exit(1);
+        }
+
+        String carattere, occorenze_String, dirName, fileName;
+        int occorenze, response, fileNumber, nread, buffer_size = 4096;
+        long fileLength;
+        boolean esiste;
+        FileOutputStream fileStream;
+        byte[] byteBuffer = new byte[buffer_size];
+
+        // CICLO INTERAZIONE CLIENT
+        try {
+            System.out.println("Operazioni: 1: conta occorrenze, 2: trasferisci file; ^D per terminare: \n");
+
+            while ((buffer = stdIn.readLine()) != null) { // ciclo di lettura da tastiera
+
+                if (buffer.equals("1")) {
+                    outSock.writeChar('1'); // invio operazione
+                    System.out.println("[DEBUG] Inviato: " + buffer);
+
+                    System.out.println("Inserire carattere: ");
+                    carattere = stdIn.readLine();
+                    System.out.println("Inserire occorrenze: ");
+                    occorenze_String = stdIn.readLine();
+
+                    try {
+                        occorenze = Integer.parseInt(occorenze_String);
+                    } catch (NumberFormatException e) {
+                        System.err.println("l'input non è un intero");
+                        System.out
+                                .println("Operazioni: 1: conta occorrenze , 2: trasferisci file; ^D per terminare: \n");
+                        continue;
+                    }
+
+                    outSock.writeChar(carattere.charAt(0));
+                    outSock.writeInt(occorenze);
+
+                    buffer = inSock.readUTF(); // attesa risposta
+                    System.out.println("[DEBUG] Ricevuto: " + buffer);
+
+                    response = inSock.readInt();
+                    System.out.println("Trovate " + response + " linee con occorrenze del carattere " + carattere);
+
+                } else if (buffer.equals("2")) {
+                    outSock.writeChar('2'); // invio operazionw
+                    System.out.println("[DEBUG] Inviato: " + buffer);
+                    System.out.println("inserire direttorio: ");
+                    dirName = stdIn.readLine();
+
+                    outSock.writeUTF(dirName);
+
+                    esiste = inSock.readBoolean();
+
+                    if (esiste) {
+                        fileNumber = inSock.readInt();
+                        for (int i = 0; i < fileNumber; i++) {
+                            fileName = inSock.readUTF();
+
+                            fileLength = inSock.readLong();
+
+                            fileStream = new FileOutputStream(fileName);
+
+                            while (fileLength > 0) {
+                                nread = inSock.read(byteBuffer, 0,
+                                        fileLength > buffer_size ? buffer_size : (int) fileLength);
+                                fileStream.write(byteBuffer, 0, nread);
+                                fileLength -= nread;
+                            }
+
+                            System.out.println("ricevuto: " + fileName);
+                            fileStream.close();
+                        }
+                    }
+
+                    buffer = inSock.readUTF(); // attesa risposta
+                    System.out.println("[DEBUG] Ricevuto: " + buffer);
+                } else {
+                    System.out.println("[ERROR] Operazione non riconosciuta\n");
+                }
+                System.out.println("Operazioni: 1: conta occorrenze, 2: trasferisci file; ^D per terminare: \n");
+            } // end while
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Errore nella comunicazione");
+            System.exit(1);
+        }
+
+        // CHIUSURA SOCKET
+        try {
+            socket.shutdownInput(); // chiusura stream di input
+            socket.shutdownOutput(); // chiusura stream di output
+            socket.close(); // chiusura socket
+        } catch (IOException e) {
+            System.out.println("[ERROR] Problemi nella chiusura della socket: ");
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+}
---
+++ Prove_Esame/Java Socket/08012020_Java/StreamThread.java
@@ -0,0 +1,165 @@
+import java.io.*;
+import java.net.*;
+
+public class StreamThread extends Thread {
+
+    private Socket clientSocket = null; // socket per comunicare con il client
+    private String[][] dati; // struttura dati condivisa tra i thread
+
+    public StreamThread(Socket socket, String[][] dati) {
+        this.clientSocket = socket;
+        this.dati = dati;
+    }
+
+    public StreamThread(Socket socket) {
+        this.clientSocket = socket;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("[DEBUG] Attivazione figlio: " + Thread.currentThread().getName());
+
+        DataInputStream inSock; // stream di input
+        DataOutputStream outSock; // stream di output
+
+        // CREAZIONE STREAM INPUT E OUTPUT
+        try {
+            inSock = new DataInputStream(clientSocket.getInputStream());
+            outSock = new DataOutputStream(clientSocket.getOutputStream());
+        } catch (IOException ioe) {
+            System.out.println("[ERROR] Problemi nella creazione degli stream di input/output ");
+            ioe.printStackTrace();
+            return;
+        }
+
+        // CORPO DELLA THREAD
+
+        char carattere;
+        String dirName;
+        int occorrenze, occ_linea, res, cread, fileNumber, nRead;
+        File currDir, dir;
+        Boolean found;
+        FileReader fr;
+        FileInputStream fileStream;
+        long byteNumber;
+        byte[] byteBuffer = new byte[4096];
+
+        try {
+            while (true) {
+                System.out.println("[DEBUG] pre ricezione");
+                char service = inSock.readChar();
+                System.out.println("[DEBUG]ricevuto richiesta di servizio " + service);
+
+                if (service == '1') {
+                    System.out.println("[DEBUG] eseguo servizio 1");
+                    outSock.writeUTF("eseguo servizio 1");
+                    // restiutuisce il numero di righe che contengono le occorrenze richieste di un
+                    // determinato carattere
+                    // leggo gli input da socket
+                    carattere = inSock.readChar();
+                    occorrenze = inSock.readInt();
+
+                    System.out.println(
+                            "ricevuto carattere: " + carattere + " e numero di occorrenze richieste: " + occorrenze);
+
+                    // creao direttorio corrente
+                    currDir = new File("./direttorio");
+                    res = 0; // response
+                    found = false;
+
+                    for (File f : currDir.listFiles()) { // leggo 1 file alla volta del direttorio
+                        if (f.isFile() && f.canRead() && f.getName().endsWith(".txt")) { // se il file è leggibile e di
+                                                                                         // testo
+                            fr = new FileReader(f);
+
+                            occ_linea = 0;
+                            while ((cread = fr.read()) != -1) { // finchè leggo il file
+                                if (cread == '\n') { // se vado a capo resetto i conteggi della linea
+                                    found = false;
+                                    occ_linea = 0;
+                                } else if (cread == carattere && !found) { // se trovo un cararttere uguale a richiesto
+                                    occ_linea++; // aggiorno le occorrenze della linea
+
+                                    if (occ_linea == occorrenze) { // se raggiungo le occorrenze richieste
+                                        found = true; // trovo una linea "corretta"
+                                        res++; // aggiungo uno al conteggio della risposta
+                                    }
+                                }
+                            }
+                            fr.close(); // chiudo il reader
+                        }
+                    }
+                    System.out.println("[DEBUG] termino servizio 1, invio risposta: " + res);
+                    outSock.writeInt(res); // invio la risposta
+
+                    // FINE
+                } else if (service == '2') {
+                    System.out.println("[DEBUG] eseguo servizio 1");
+                    // invio i file binari nel direttorio richiesto dal cliente
+                    dirName = inSock.readUTF();
+
+                    dir = new File(dirName);
+
+                    if (!dir.exists() || !dir.canRead() || !dir.isDirectory()) {
+                        outSock.writeBoolean(false);
+                        continue;
+                    }
+
+                    outSock.writeBoolean(true);
+
+                    fileNumber = 0;
+                    for (File f : dir.listFiles()) {
+                        if (f.canRead() && f.isFile() && !f.getName().endsWith(".txt")) {
+                            fileNumber++;
+                        }
+                    }
+
+                    // Invio numero file
+                    outSock.writeInt(fileNumber);
+
+                    for (File f : dir.listFiles()) {
+                        if (f.canRead() && f.isFile() && !f.getName().endsWith(".txt")) {
+                            outSock.writeUTF(f.getName());
+                            fileStream = new FileInputStream(f);
+
+                            byteNumber = f.length();
+
+                            outSock.writeLong(byteNumber);
+
+                            while (byteNumber > 0) {
+                                nRead = fileStream.read(byteBuffer);
+                                outSock.write(byteBuffer, 0, nRead);
+
+                                byteNumber -= nRead;
+                            }
+
+                            fileStream.close();
+                        }
+                    }
+                    // FINE
+                    System.out.println("[DEBUG] termino servizio 2");
+                }
+            } // fine ciclo interazione
+
+        } catch (EOFException e) {
+            System.out.println("Terminazione. (Thread=" + getName() + ")");
+        } catch (SocketTimeoutException e) {
+            System.out.println("[ERROR]: Socket timed out. (Thread=" + getName() + ")");
+            e.printStackTrace();
+        } catch (IOException e) {
+            System.out.println("[ERROR]: Lettura/Scrittura su stream fallita. (Thread=" + getName() + ")");
+            e.printStackTrace();
+        } catch (Exception e) {
+            System.out.println("[ERROR]: Errore irreversibile. (Thread=" + getName() + ")");
+            e.printStackTrace();
+        } finally {
+            System.out.println("[DEBUG]: Terminazione server thread. (Thread=" + getName() + ")");
+            try {
+                clientSocket.close();
+            } catch (IOException e) {
+                System.out.println("[ERROR]: Chiusura socket fallita. (Thread=" + getName() + ")");
+                e.printStackTrace();
+            }
+        }
+    }
+}
