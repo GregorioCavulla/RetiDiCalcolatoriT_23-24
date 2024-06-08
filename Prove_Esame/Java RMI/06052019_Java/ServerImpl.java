13cef9737317b9854491255bb21f507826110c75
GregorioCavulla <ghigo2000.mr@gmail.com>
2024-06-08 16:45:11 +0200

    RMI

--- Prove_Esame/Java RMI/06052019_Java/ServerImpl.java
+++ Prove_Esame/Java RMI/06052019_Java/ServerImpl.java
@@ -1,5 +1,12 @@
 package RMI;
 
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 /**
  * ServerImple.java
  * 	Implementa i servizi dichiarati in RemOp.
@@ -68,21 +75,96 @@ public class ServerImpl extends UnicastRemoteObject implements RMI_interfaceFile
 	@Override
 	public String[] lista_file(String dirName) throws RemoteException { // Implementazione del metodo 1 dichiarato in RMI_interfaceFile
 
-		int esito = -1;
 		System.out.println("[DEBUG] lista_file ");
 
-		if (id == null){
-			return esito;
+		File dir = new File(dirName); // Creazione di un oggetto File per il direttorio richiesto
+
+		if(!dir.isDirectory() || !dir.exists() || !dir.canRead()){
+			throw new RemoteException("Il direttorio non contiene file");
 		}
 
-		return esito;
+		int numeroFile = dir.listFiles().length; // Numero di file presenti nel direttorio
+
+		String temp[] = new String[numeroFile]; // Array temporaneo per memorizzare i nomi dei file che soddisfano i requisiti
+		String resulString[]; // Array di stringhe che conterrà i nomi dei file che soddisfano i requisiti
+		char fileName[]; // Array di caratteri per memorizzare il nome del file
+
+		int consonanti, j=0;
+
+		for(File f : dir.listFiles()){ //per ogni file nel direttorio, verifico che il nome del file contenga più di 3 consonanti, in tal caso lo memorizzo nell'array temp
+			fileName = f.getName().toLowerCase().toCharArray();
+			consonanti = 0;
+
+			for(int i=0; i<fileName.length; i++){
+				if(fileName[i]!='a'||fileName[i]!='e'||fileName[i]!='i'||fileName[i]!='o'||fileName[i]!='u'){
+					consonanti ++;
+					if(consonanti>3){
+						temp[j++]=f.getName();
+						break;
+					}
+				}
+			}
+		}
+
+		resulString = new String[j];
+
+		for(int i=0; i<resulString.length; i++){ // Copio i nomi dei file che soddisfano i requisiti nell'array resulString
+			resulString[i] = temp[i];
+		}
+
+		return resulString;
+
 	}
 
 	@Override
 	public  int numerazione_righe(String fileName) throws RemoteException {  // Implementazione del metodo 2 dichiarato in RMI_interfaceFile
 		
-		int esito = -1;
+		int esito = -1; // Valore di ritorno del metodo
+		int riga = 0; // Contatore delle righe del file
+		File f = new File(fileName); // Creazione di un oggetto File per il file richiesto
+		File outFile, inFile; // File di output e file di input per la scrittura e la lettura del file modificato
+		FileWriter outFileWriter; // Oggetto FileWriter per la scrittura del file di output
+		String line; // Stringa per memorizzare la riga letta dal file
+		String outFileName = fileName.substring(0, (fileName.length()-4)) + "_modified.txt"; // Nome del file di output
+		BufferedReader br;
+
 		System.out.println("[DEBUG] numerazione_righe ");
+
+		if(!f.exists() || !f.isFile() || !f.canRead()){
+			return esito;
+		}
+
+		
+		try{
+			br = new BufferedReader(new FileReader(f));
+			outFile = new File(outFileName);
+			outFileWriter = new FileWriter(outFile);
+		}catch (FileNotFoundException e){
+				return esito;
+		} catch (IOException e) {
+			throw new RemoteException(e.toString());
+		}
+		
+		esito = 0;
+
+		try{
+			riga = 0;
+			while((line = br.readLine()) != null){
+				riga ++;
+				if(riga%2 != 0){
+					outFile.write(riga + " "+line+"\n");
+					esito ++;
+				}
+			}
+			br.close();
+			outFile.close();
+
+			inFile = new File(fileName);
+			outFile.renameTo(inFile);
+			inFile.delete();
+		}catch(IOException e){
+			throw new RemoteException(e.toString());
+		}
 
 		return esito;
 	}
